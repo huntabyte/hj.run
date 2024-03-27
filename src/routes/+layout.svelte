b98f29454db313192007b2e6477a572727519660
Hunter Johnston <johnstonhuntera@gmail.com>
2024-03-26 20:38:42 -0400

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,12 @@
 	const navigation = [
 		{ name: 'Projects', href: '/projects' },
 		{ name: 'Posts', href: '/posts' },
-		{ name: 'Stuff', href: '/stuff' },
+		{ name: 'Uses', href: '/uses' },
 		{ name: 'Contact', href: '/contact' }
 	];
 </script>
 
-<div class="mx-auto max-w-3xl">
+<div class="mx-auto flex min-h-screen max-w-3xl flex-col">
 	<nav class="flex items-center justify-between">
 		<a href="/" class="py-6 text-xl font-bold"> HJ </a>
 		<div class="flex items-center gap-5">
@@ -21,4 +21,7 @@
 	<main class="py-10">
 		<slot />
 	</main>
+	<footer class="mt-auto py-6 text-center">
+		<p>© 2024 Hunter Johnston</p>
+	</footer>
 </div>
