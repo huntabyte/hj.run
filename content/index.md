bfe017f251849e15199c88242404b2a6fd577d6d
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-10 16:10:06 -0400

    move homepage stuff to markdown

---
+++ content/index.md
@@ -0,0 +1,15 @@
+I've been working professionally in tech for almost 10 years and have been passionate about it for as long as I can remember.
+
+I really enjoy building and maintaining open source projects for the Svelte ecosystem.
+
+Some of the more notable projects I've built or helped build and maintain are:
+
+- [shadcn-svelte](https://shadcn-svelte.com)
+- [Melt UI](https://melt-ui.com)
+- [Bits UI](https://bits-ui.com)
+- [Formsnap](https://formsnap.dev)
+- [PaneForge](https://paneforge.com)
+
+I also gain fulfillment in sharing what I learn with others, mostly in the form of [video content](https://youtube.com/@huntabyte). I'm hoping that having this site will encourage me to share more [written](/articles) content as well.
+
+When I'm not doing things on the internet, I enjoy spending time with my beautiful fiancée and our two dogs. We love trying out new breakfast spots, especially if they have good brew (coffee).
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,11 @@
 <script lang="ts">
+	// @ts-ignore
+	import HomeContent from '../../content/index.md';
 </script>
 
 <section
 	class="prose prose-stone mt-4 md:prose-lg prose-a:underline prose-a:decoration-stone-400 prose-a:underline-offset-4 prose-li:marker:text-stone-400"
 >
 	<h1 class="not-prose text-2xl font-medium text-stone-950">I'm Hunter, aka Huntabyte.</h1>
-	<p>
-		I've been working professionally in tech for almost 10 years and have been passionate about it
-		for as long as I can remember.
-	</p>
-	<p>I really enjoy building and maintaining open source projects for the Svelte ecosystem.</p>
-	<p>Some of the more notable projects I've built or helped build and maintain are:</p>
-	<ul>
-		<li>
-			<a href="https://shadcn-svelte.com" target="_blank"> shadcn-svelte </a>
-		</li>
-		<li>
-			<a href="https://melt-ui.com" target="_blank"> Melt UI </a>
-		</li>
-		<li>
-			<a href="https://bits-ui.com" target="_blank"> Bits UI </a>
-		</li>
-		<li>
-			<a href="https://formsnap.dev" target="_blank"> Formsnap </a>
-		</li>
-		<li>
-			<a href="https://paneforge.com" target="_blank"> PaneForge </a>
-		</li>
-	</ul>
-	<p>
-		I also gain fulfillment in sharing what I learn with others, mostly in the form of <a
-			href="https://youtube.com/@huntabyte"
-			target="_blank"
-		>
-			video content
-		</a>. I'm hoping that having this site will encourage me to share more
-		<a href="/articles">written</a> content as well.
-	</p>
-	<p>
-		When I'm not doing things on the internet, I enjoy spending time with my beautiful fiancée and
-		our two dogs. We love trying out new breakfast spots, especially if they have good brew
-		(coffee).
-	</p>
+	<HomeContent />
 </section>
