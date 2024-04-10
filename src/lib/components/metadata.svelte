9fd3cea7fb9567ef75e394198110a09594fab1ab
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-10 17:09:19 -0400

    update meta

--- src/lib/components/metadata.svelte
+++ src/lib/components/metadata.svelte
@@ -4,7 +4,7 @@
 
 	export let title: string = siteConfig.name;
 
-	$: title = $page.data?.title ? `${$page.data.title} - ${siteConfig.name}` : siteConfig.name;
+	$: title = $page.data?.title ? `${$page.data.title}` : siteConfig.name;
 	$: description = $page.data?.description ?? siteConfig.description;
 </script>
 
