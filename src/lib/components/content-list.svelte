d26e9b9a8fa7fb01264421b1267198edf4dec971
Hunter Johnston <johnstonhuntera@gmail.com>
2024-04-11 12:09:48 -0400

    more style tweaks

--- src/lib/components/content-list.svelte
+++ src/lib/components/content-list.svelte
@@ -5,11 +5,11 @@
 <div class="flex items-center justify-center">
 	<ul class="flex w-full max-w-xl flex-col gap-5">
 		{#each content as { title, slugFull, date }}
-			<li class="flex w-full items-start gap-4 md:gap-12">
+			<li class="flex w-full items-center gap-4 md:gap-12">
 				<a class="text-left font-medium" href={slugFull}>
 					{title}
 				</a>
-				<span class="ml-auto w-[110px] shrink-0 border text-base text-stone-950">{date}</span>
+				<span class="ml-auto w-[110px] shrink-0 border text-sm text-stone-600/85">{date}</span>
 			</li>
 		{/each}
 	</ul>
--- src/routes/finds/[...slug]/+page.svelte
+++ src/routes/finds/[...slug]/+page.svelte
@@ -4,6 +4,6 @@
 
 <article class="markdown">
 	<h1 class="!mb-0 !text-3xl">{data.title}</h1>
-	<span class="!mt-0 mb-4 text-base">{data.metadata.date}</span>
+	<span class="!mt-0 mb-4 text-sm text-stone-600/85">{data.metadata.date}</span>
 	<svelte:component this={data.component} />
 </article>
