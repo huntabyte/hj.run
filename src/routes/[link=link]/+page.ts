c2477c21d0da9ebd1ba8b0369dc6632c62fdba39
Hunter Johnston <johnstonhuntera@gmail.com>
2024-06-30 13:40:18 -0400

    cleanup

--- src/routes/[link=link]/+page.ts
+++ src/routes/[link=link]/+page.ts
@@ -3,9 +3,7 @@ import { isLink, linkMap } from '../../params/link.js';
 
 export async function load(event) {
 	const link = event.params.link.toLowerCase();
-	if (!isLink(link)) {
-		redirect(302, '/');
-	}
+	if (!isLink(link)) redirect(302, '/');
 
 	redirect(302, linkMap[link]);
 }
